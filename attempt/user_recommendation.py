bcfaa416f45962ba5f3701e3ee8d99e0c4f23944
Preetesh Verma <verma.preetesh21@gmail.com>
2020-10-25 16:42:10 +0530

    Added files

---
+++ attempt/user_recommendation.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import json 
+from keras.models import model_from_json
+import numpy as np 
+ 
+ratings_df=pd.read_csv("ratings.csv")
+# load json and create model
+json_file = open('./model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("./model.h5")
+print("Loaded model from disk")
+
+b_id =list(ratings_df.book_id.unique())
+b_id.remove(10000)
+#Making recommendations for user 100
+book_arr = np.array(b_id) #get all book IDs
+user = np.array([100 for i in range(len(b_id))])
+pred = loaded_model.predict([book_arr, user])
+pred = pred.reshape(-1) #reshape to single dimension
+pred_ids = (-pred).argsort()[0:5]
+print(pred_ids)
+
+with open("web_book_data.json", "r") as read_it: 
+     data = json.load(read_it) 
+
+print(data[pred_ids[0]])
+d=[]
+for i in pred_ids:
+        d.append(data[i])
+print(d)
+
