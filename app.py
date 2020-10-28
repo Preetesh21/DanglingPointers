678e4cceb908eb523f8d6056fd7f8bc4fc61f0e2
Preetesh Verma <verma.preetesh21@gmail.com>
2020-10-28 20:43:34 +0530

    Added object linkage

--- app.py
+++ app.py
@@ -41,8 +41,7 @@ def bolly_predict():
 def fashion_predict():
     ans=news()
     print(ans)
-    return render_template('index4.html', objects = ans)
-    return str(ans)
+    return render_template('index4.html',objects=ans)
 
 if __name__ == "__main__":
     app.run(debug=True)
