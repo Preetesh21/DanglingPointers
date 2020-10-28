d772914e67cf55b483f7597d76c445516fe27ee2
SoniGarima <2018csb1089@iitrpr.ac.in>
2020-10-28 09:48:12 +0530

    Scraped insta's top fashion hashtags

--- scrape_from_instagram.py
+++ scrape_from_instagram.py
@@ -9,13 +9,21 @@ def scrap_latest_fashion(links_list):
     driver = webdriver.Chrome("/home/garima/chromedriver")
     for url in links_list:
         driver.get(url)
-        players = driver.find_elements_by_xpath('//img[@class="img-responsive"]')
+        players = driver.find_elements_by_xpath('//img[@class="FFVAD"]')
         for p in players:
             Links.append(str(p.get_attribute('src')))
-            Descriptions.append(str(p.get_attribute('title')))
+            Descriptions.append(str(p.get_attribute('alt')))
     df = pd.DataFrame({'Product URL':Links, 'Descriptions':Descriptions}) 
-    df.to_csv('products_latest.csv', index=False, encoding='utf-8')
+    df.to_csv('products_from_instagram.csv', index=False, encoding='utf-8')
 
-core_url = "www.instagram.com"
+core_url = "https://www.instagram.com/explore/tags/"
+hashtags = ["fashionblogger","fashionstyle","fashiontrends","styleblogger","fashionmodel","latesttrends","bollywoodfashion","latestcelebrityfashion"]
+links_list = []
+for tag in hashtags:
+    new_url = core_url + tag
+    links_list.append(new_url)
+scrap_latest_fashion(links_list)
+
+
 
 
